75c776b97efb3a0a2ab7e5517ad1fe388de2f242
SAsimRaza <asimraza2016@gmail.com>
2023-03-30 13:59:20 +0500

    feat: update basic test environment hardhat

--- test/NftStore.test.ts
+++ test/NftStore.test.ts
@@ -1,11 +1,13 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Token", function () {
-  it("Should return name Token", async function () {
-    // const Token = await ethers.getContractFactory("Token");
-    // const token = await Token.deploy();
-    // await token.deployed();
-    // expect(await token.name()).to.equal("Token");
+describe("NftStore", function () {
+  it("Should return nft store name", async function () {
+    const NFTStore = await ethers.getContractFactory("NftStore");
+    const nftStore = await NFTStore.deploy(
+      "0xef045a554cbb0016275E90e3002f4D21c6f263e1"
+    );
+    await nftStore.deployed();
+    expect(await nftStore.name()).to.equal("NFT Store");
   });
 });
